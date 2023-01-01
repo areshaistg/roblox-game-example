9394d79b3c883f9e727938492bea5c6c5c01bc91
areshaistg <areshaistg@gmail.com>
2023-01-01 22:24:29 +0800

    rename setupScreen to setupInterface

--- src/shared/setupInterface.lua
+++ src/shared/setupInterface.lua
@@ -6,13 +6,13 @@ local Player = game:GetService("Players").LocalPlayer
 local Roact = require(ReplicatedStorage.Packages.roact)
 local RoactHooks = require(ReplicatedStorage.Packages["roact-hooks"])
 
---|| Setup Screen ||--
+--|| Setup Interface ||--
 local UI = ReplicatedStorage.Common.ui
 local App = require(UI.App)
 
-local function setupScreen(world, state)
+local function setupInterface(world, state)
     local app = Roact.createElement(App)
     Roact.mount(app, Player:WaitForChild("PlayerGui"))
 end
 
-return setupScreen
+return setupInterface
